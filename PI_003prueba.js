ee3efec2872607f8653df2ac065e937348a8906e
Niplhu <alexutiel01@gmail.com>
2025-12-05 20:54:23 +0100

    Añadir uttilidadesDeposito y mejora claseDeposito

--- PI_003prueba.js
+++ PI_003prueba.js
@@ -10,11 +10,19 @@ class Deposito {
   Vaciar(cantidad) {
     this.litros -= cantidad;
   }
+
+  // NUEVO: función para rellenar el depósito
+  Rellenar(cantidad) {
+    this.litros += cantidad;
+  }
 }
 
 let depositoMunicipal = new Deposito("Agua", 100000, "Toledo", 2007);
 
 // Se vacian 5000 litros
 depositoMunicipal.Vaciar(5000);
+
+// Se rellenan 2000 litros
+depositoMunicipal.Rellenar(2000);
 
 console.log(depositoMunicipal);
